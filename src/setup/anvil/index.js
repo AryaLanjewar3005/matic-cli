9560feeead2654564cce89fe5160e049a4094287
Arya Lanjewar <102943033+AryaLanjewar3005@users.noreply.github.com>
2025-01-30 17:27:01 +0530

    dependency script now transfers fund from 2nd anvil account to first signer's accound

--- src/setup/anvil/index.js
+++ src/setup/anvil/index.js
@@ -9,16 +9,11 @@ import { processTemplateFiles } from '../../lib/utils.js'
 import { getDefaultBranch } from '../helper.js'
 import { Contracts } from '../contracts/index.js'
 import { getRemoteStdio } from '../../express/common/remote-worker.js'
-import { createAccountsFromMnemonics } from '../../lib/utils.js'
 
 export class Anvil {
   constructor(config, options = {}) {
     this.config = config
     this.mnemonic = config.mnemonic
-    console.log("This is from config.anvilAccounts : ", config.anvilAccounts)
-    this.deployerAccount = createAccountsFromMnemonics(this.mnemonic, 1)
-    console.log(`Deployer's account : ${this.deployerAccount[0].privateKey}`)
-    this.deployerPrivateKey = this.deployerAccount[0].privateKey 
 
     this.dbName = options.dbName || 'anvil-db'
     this.serverPort = options.serverPort || 9545
